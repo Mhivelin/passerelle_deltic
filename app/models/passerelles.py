0c950f23a994f15f8f49778e0ce8762ab815e9ea
Marius Hivelin <marius.hivelin@gmail.com>
2024-07-17 10:02:06 +0200

    fix paiement

--- app/models/passerelles.py
+++ app/models/passerelles.py
@@ -204,18 +204,17 @@ def p_remonte_paiement_sellsy_zeendoc(IdPasserelleClient):
     )["Valeur"]
 
     for doc in paiddoc:
-        if "payments" not in doc:
-            logger.error(f"Missing 'payments' field in document: {doc['id']}, {doc.get('number')}")
+        try:
+            payments = doc["payments"]
+            if not payments.get("data"):
+                logger.warning(f"No payments data in document: {doc['id']}, {doc.get('number')}")
+                paid_at = doc["due_date"]
+            else:
+                last_payment = max(payments["data"], key=lambda x: x["paid_at"])
+                paid_at = last_payment["paid_at"].split("T")[0]
+        except KeyError:
+            logger.warning(f"Missing 'payments' field in document: {doc['id']}, {doc.get('number')}")
             paid_at = doc["due_date"]
-
-
-        payments = doc["payments"]
-        if not payments.get("data"):
-            logger.warning(f"No payments data in document: {doc['id']}, {doc.get('number')}")
-            paid_at = doc["due_date"]
-        else:
-            last_payment = max(payments["data"], key=lambda x: x["paid_at"])
-            paid_at = last_payment["paid_at"].split("T")[0]
 
         res = zeendoc.update_doc_paiement_by_num_facture(doc["number"], index, paid_at)
         logger.info(f"Updated document in Zeendoc: {doc['number']} with response: {res}")
@@ -225,6 +224,7 @@ def p_remonte_paiement_sellsy_zeendoc(IdPasserelleClient):
         logger.info(f"Updated smart tags in Sellsy for document: {doc['number']} with response: {res}")
 
     logger.info("Routine completed successfully.")
+
 
 
 
