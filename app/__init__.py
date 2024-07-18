d136b55d6ec39277d21ee3255dddba6ea8e31946
Marius Hivelin <marius.hivelin@gmail.com>
2024-07-18 15:16:19 +0200

    fix passerelle

--- app/__init__.py
+++ app/__init__.py
@@ -13,9 +13,6 @@ import dotenv
 from flask import Flask, render_template, request
 # from flask_jwt_extended import JWTManager
 from flask_login import LoginManager
-from prometheus_client import Counter, make_wsgi_app
-# from prometheus_flask_exporter import PrometheusMetrics
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 from app.extensions import db
 from app.models.database import create_database
@@ -23,24 +20,6 @@ from app.models.user import User
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
-# Define Prometheus counters for logs
-INFO_LOG_COUNT = Counter("info_log_count", "Number of info log entries")
-WARNING_LOG_COUNT = Counter("warning_log_count", "Number of warning log entries")
-ERROR_LOG_COUNT = Counter("error_log_count", "Number of error log entries")
-
-
-class PrometheusLoggingHandler(logging.Handler):
-    """
-    Création d'un handler pour les logs qui incrémente les compteurs Prometheus.
-    """
-
-    def emit(self, record):
-        if record.levelno == logging.INFO:
-            INFO_LOG_COUNT.inc()
-        elif record.levelno == logging.WARNING:
-            WARNING_LOG_COUNT.inc()
-        elif record.levelno == logging.ERROR:
-            ERROR_LOG_COUNT.inc()
 
 
 def configure_logs(app):
@@ -63,10 +42,6 @@ def configure_logs(app):
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
-    # Add Prometheus logging handler
-    prometheus_handler = PrometheusLoggingHandler()
-    prometheus_handler.setFormatter(formatter)
-    app.logger.addHandler(prometheus_handler)
 
     app.logger.info("Configuration des logs terminée.")
 
@@ -96,9 +71,7 @@ def create_app():
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
     db.init_app(app)
 
-    # Configure Prometheus metrics exporter
-    # metrics = PrometheusMetrics(app)
-    app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
+
 
     # Configure logs
     configure_logs(app)
